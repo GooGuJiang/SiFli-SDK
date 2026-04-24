89e55c6b0e780de5889e46f72e61204824173f18
Xiaoming Xi <xiaomingxi@sifli.com>
2026-04-24 09:50:01 +0800

    [chore][example][pm/coremark] Remove unused partition table in coremark example to fix build error for board eh-lb525
    ext-redmine: #101, REDMINE-101
    
    [Description in detail]
    As some boards don't define kvdb_ble partition, it leads to build error with this part table
    
    Change-Id: I4bd473c7ee8292b18d3cbac6cdbff36aa457796f

--- example/pm/coremark/project/hcpu/sf32lb52x/custom_mem_map.h
+++ example/pm/coremark/project/hcpu/sf32lb52x/custom_mem_map.h
@@ -12,11 +12,6 @@
 #endif /* USING_PARTITION_TABLE */
 
 #ifndef USING_PTAB_V3
-#define FAL_PART_TABLE \
-{ \
-    {FAL_PART_MAGIC_WORD,       "dfu",      NOR_FLASH2_DEV_NAME,    KVDB_DFU_REGION_OFFSET,   KVDB_DFU_REGION_SIZE, 0}, \
-    {FAL_PART_MAGIC_WORD,       "ble",      NOR_FLASH2_DEV_NAME,    KVDB_BLE_REGION_OFFSET,   KVDB_BLE_REGION_SIZE, 0}, \
-}
 
 /* Match the legacy CoreMark 52x HCPU windowing on v1/v2 PTAB boards. */
 #undef LPSYS_RAM_SIZE
